c44e5cdba4c4a9515a3d13d445ef2962897b35ef
Bruno Pavasović <50275339+Brunopavasovic@users.noreply.github.com>
2022-01-20 14:51:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,49 +3,154 @@ let changeBgToWhite = document.getElementById('theme-changer-sun');
 let del = document.querySelector('remove');
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
+let showItemsLeft = document.getElementById('items-left');
+let showCompletedEl = document.getElementById('show-completed');
+let showAllTodos = document.getElementById('show-all');
+let showActiveTodods = document.getElementById('show-active');
+let clearCompletedTodos = document.getElementById('clear-completed');
 
+const arr =
+[
+  //{ content: 'Groceries', done: false, id: 'b3a678e10e078' },
+  //{ content: 'Groceries', done: false, id: 'b3a678e10e078' },
+];
 
-changeBgToDark.addEventListener("click" ,() => {
-   document.querySelector('body').classList = [changeBgToDark.click ? 'dark-theme' : 'white-theme'];
-   changeBgToDark.style.display = "none";
-   changeBgToWhite.style.display = "block";
-  
+const uniqueId = Math.random().toString(16).slice(2);
+
+changeBgToDark.addEventListener("click", () => {
+  document.querySelector('body').classList = [changeBgToDark.click ? 'dark-theme' : 'white-theme'];
+  changeBgToDark.style.display = "none";
+  changeBgToWhite.style.display = "block";
+
 });
 
 changeBgToWhite.addEventListener("click", () => {
-   document.querySelector('body').classList = [changeBgToWhite.click ? 'white-theme' : 'dark-theme'];
-   changeBgToWhite.style.display = "none";
-   changeBgToDark.style.display = "block";
+  document.querySelector('body').classList = [changeBgToWhite.click ? 'white-theme' : 'dark-theme'];
+  changeBgToWhite.style.display = "none";
+  changeBgToDark.style.display = "block";
 });
 
-input.addEventListener('keypress' , (e) => {
-  if(e.charCode === 13 && input.value.length > 0) {
-      createTodo(input.value);
-      input.value ="";
+input.addEventListener('keypress', (e) => {
+  if (['Enter', 'NumpadEnter'].includes(e.code) && input.value.length > 0) {
+    createTodo(input.value);
+    input.value = "";
   }
 });
 
-
 const createTodo = (txt) => {
-    let elem = document.createElement('li');
-    elem.innerHTML = ` <label class="list-container">
-          <input type="checkbox">
-          <span class="txt">${txt}</span>
-          <span class="checkmark"></span>
-          </label>
-            <span class="remove"></span>`;
-            ul.append(elem);
+  let elem = document.createElement('li');
+  elem.innerHTML = `
+    <label class="list-container">
+      <input type="checkbox" class="checkbox-done">
+      <span class="txt">${txt}</span>
+      <span class="checkmark"></span>
+    </label>
+    <span class="remove"></span>
+  `;
+  
+  ul.append(elem);
+  showNum();
+  filterItemsOnChange();
+
+ arr.push({
+    content: txt , done: '' ,  id: '1'
+ });
+  
 }
- 
+
+
+
+
 const deleteTodo = (elem) => {
   elem.remove();
+  showNum();
 }
 
-ul.addEventListener('click' , (event) => {
-    if(event.target.classList.contains('remove')) {
-        deleteTodo(event.target.parentElement);
-    }
+
+
+ul.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove')) {
+    deleteTodo(event.target.parentElement);
+    showNum();
+
+  }
 })
 
-  
+const showNum = () => {
+  console.log('showNum');
+  let ckbox = document.querySelectorAll('.checkbox-done');
+  let checkedBox = document.querySelectorAll('.checkbox-done:checked');
+  let notChecked = document.querySelectorAll('.checkbox-done:not(:checked)');
+  /* showItemsLeft.innerText = `Items left: ${ckbox.length}`; */
+  document.querySelector('#show-all .counter').innerText = `(${ckbox.length})`;
+  document.querySelector('#show-completed .counter').innerText = `(${checkedBox.length})`;
+  document.querySelector('#show-active .counter').innerText = `(${notChecked.length})`;
+
+}
+
+// dodaj evt listnr na chbox -change
+
+const filterItemsOnChange = () => {
+  document.querySelectorAll('.checkbox-done').forEach((checkbox) => {
+    checkbox.addEventListener('change', showNum);
+  })
+};
+
+
+const showAll = () => {
+  document.querySelectorAll('.checkbox-done').forEach((checkbox) => {
+    const item = checkbox.closest('li');
+    item.style.display = 'block';
+
+  })
+};
+
+
+
+const showActive = () => {
+  document.querySelectorAll('.checkbox-done').forEach((checkbox) => {
+    const item = checkbox.closest('li');
+    item.style.display = checkbox.checked ? 'none' : 'block';
+
+
+
+
+  }
+  )
+};
+
+
+const showCompleted = () => {
+  document.querySelectorAll('.checkbox-done').forEach((checkbox) => {
+    const item = checkbox.closest('li');
+    item.style.display = checkbox.checked ? 'block' : 'none';
+  })
+};
+
+
+const clearCompleted = () => {
+  document.querySelectorAll('.checkbox-done').forEach((checkbox) => {
+    const item = checkbox.closest('li');
+    if (checkbox.checked) {
+      deleteTodo(item);
+      showNum();
+    }
+    else {
+      item.style.display = 'block';
+    }
+  })
+}
+
+showCompletedEl.addEventListener('click', showCompleted);
+showAllTodos.addEventListener('click', showAll);
+showActiveTodods.addEventListener('click', showActive);
+clearCompletedTodos.addEventListener('click', clearCompleted);
+
+
+
+createTodo('Groceries');
+createTodo('Work');
+showNum();
+filterItemsOnChange();
+
 
